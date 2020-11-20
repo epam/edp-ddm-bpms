b4a6321b9506645fc569cc950bb304ed64884881
Oleh Soklakov <oleh_soklakov@epam.com>
2020-11-20 14:22:34 +0200

    [MDTUDDM-478] add bpmn integration test

---
+++ src/test/java/ua/gov/mdtu/ddm/bpms/it/BaseIT.java
@@ -0,0 +1,22 @@
+package ua.gov.mdtu.ddm.bpms.it;
+
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+public abstract class BaseIT {
+
+  @Autowired
+  protected RuntimeService runtimeService;
+  @Autowired
+  protected TaskService taskService;
+
+}
