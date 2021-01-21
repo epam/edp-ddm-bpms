a6cf580786e4ada4243e5166269c1034f75abfd3
Oleh Soklakov <oleh_soklakov@epam.com>
2021-01-21 13:19:53 +0200

    [MDTUDDM-1053] fix rebasing issue

--- business-process-management/src/main/java/ua/gov/mdtu/ddm/lowcode/bpms/camunda/config/LowcodeJerseyConfig.java
+++ business-process-management/src/main/java/ua/gov/mdtu/ddm/lowcode/bpms/camunda/config/LowcodeJerseyConfig.java
@@ -2,6 +2,7 @@ package ua.gov.mdtu.ddm.lowcode.bpms.camunda.config;
 
 import org.camunda.bpm.spring.boot.starter.rest.CamundaJerseyResourceConfig;
 import org.springframework.stereotype.Component;
+import ua.gov.mdtu.ddm.lowcode.bpms.camunda.controller.TaskPropertyController;
 import ua.gov.mdtu.ddm.lowcode.bpms.exception.mapper.UserDataValidationExceptionMapper;
 
 @Component
@@ -9,6 +10,7 @@ public class LowcodeJerseyConfig extends CamundaJerseyResourceConfig {
 
   @Override
   protected void registerAdditionalResources() {
+    register(TaskPropertyController.class);
     register(UserDataValidationExceptionMapper.class);
   }
 }
