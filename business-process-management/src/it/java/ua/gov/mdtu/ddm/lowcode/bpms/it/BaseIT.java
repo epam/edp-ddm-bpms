93e2496b06ef9965a7eed9e6e69b7acdada034fd
Oleh Soklakov <oleh_soklakov@epam.com>
2021-02-03 12:48:53 +0000

    WIP [MDTUDDM-2204] spring security based on jwt

--- business-process-management/src/it/java/ua/gov/mdtu/ddm/lowcode/bpms/it/BaseIT.java
+++ business-process-management/src/it/java/ua/gov/mdtu/ddm/lowcode/bpms/it/BaseIT.java
@@ -1,20 +1,34 @@
 package ua.gov.mdtu.ddm.lowcode.bpms.it;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.io.ByteStreams;
+import java.io.IOException;
 import javax.inject.Inject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 import org.camunda.bpm.engine.HistoryService;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.TaskService;
+import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public abstract class BaseIT {
+
+  protected final String TOKEN_HEADER = "x-access-token";
 
   @Inject
   protected RuntimeService runtimeService;
@@ -24,5 +38,47 @@ public abstract class BaseIT {
   protected TaskService taskService;
   @Inject
   protected ProcessEngine engine;
+  @Inject
+  protected ObjectMapper objectMapper;
 
+  @LocalServerPort
+  protected int port;
+  protected Client jerseyClient = JerseyClientBuilder.createClient();
+
+  protected static String validAccessToken;
+
+  @BeforeClass
+  public static void setup() throws IOException {
+    validAccessToken = new String(ByteStreams
+        .toByteArray(BaseIT.class.getResourceAsStream("/json/testuserAccessToken.json")));
+  }
+
+  protected <T> T getForObject(String url, Class<T> targetClass) throws IOException {
+    return this.getForObject(url, targetClass, validAccessToken);
+  }
+
+  protected <T> T postForObject(String url, String body, Class<T> targetClass)
+      throws JsonProcessingException {
+    return this.postForObject(url, body, targetClass, validAccessToken);
+  }
+
+  protected <T> T getForObject(String url, Class<T> targetClass, String accessToken)
+      throws IOException {
+    String jsonResponse = jerseyClient
+        .target(String.format("http://localhost:%d/%s", port, url))
+        .request(MediaType.APPLICATION_JSON)
+        .header(TOKEN_HEADER, accessToken)
+        .get().readEntity(String.class);
+    return objectMapper.readValue(jsonResponse, targetClass);
+  }
+
+  protected <T> T postForObject(String url, String body, Class<T> targetClass, String accessToken)
+      throws JsonProcessingException {
+    String jsonResponse = jerseyClient.target(String.format("http://localhost:%d/%s", port, url))
+        .request()
+        .header(TOKEN_HEADER, accessToken)
+        .post(Entity.entity(body, MediaType.APPLICATION_JSON))
+        .readEntity(String.class);
+    return objectMapper.readValue(jsonResponse, targetClass);
+  }
 }
