0a8ef4279320efbcd1eaaf8c4b7231423a579758
Ruslan_Lesyk <Ruslan_Lesyk@epam.com>
2021-03-18 17:29:33 +0200

    [MDTUDDM-2873] removed permission to execute cross-origin requests

--- business-process-management/src/main/java/ua/gov/mdtu/ddm/lowcode/bpms/config/GeneralConfig.java
+++ business-process-management/src/main/java/ua/gov/mdtu/ddm/lowcode/bpms/config/GeneralConfig.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
 import org.springframework.boot.jdbc.DatabaseDriver;
 import org.springframework.boot.json.JacksonJsonParser;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -16,30 +15,12 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.jdbc.support.DatabaseStartupValidator;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import ua.gov.mdtu.ddm.general.integration.ceph.config.CephConfig;
 
 @Configuration
 @EnableAspectJAutoProxy
 @Import(CephConfig.class)
 public class GeneralConfig {
-
-    @Bean
-    public FilterRegistrationBean<CorsFilter> processCorsFilter() {
-        var source = new UrlBasedCorsConfigurationSource();
-        var config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-
-        var bean = new FilterRegistrationBean<>(new CorsFilter(source));
-        bean.setOrder(0);
-        return bean;
-    }
 
     @Bean
     public RestTemplate restTemplate() {
