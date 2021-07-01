7f122f817798a50b2238560bb86e3c4dce4d81f5
Oleh Soklakov <oleh_soklakov@epam.com>
2021-07-01 09:50:25 +0000

    [MDTUDDM-6739] IT's for officer-create-subject-bp.bpmn

--- business-process-management/src/it/java/com/epam/digital/data/platform/bpms/it/SubjectDetailEdrRegistryConnectorDelegateIT.java
+++ business-process-management/src/it/java/com/epam/digital/data/platform/bpms/it/SubjectDetailEdrRegistryConnectorDelegateIT.java
@@ -1,39 +1,19 @@
 package com.epam.digital.data.platform.bpms.it;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.matching;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-
 import com.epam.digital.data.platform.bpms.it.camunda.bpmn.BaseBpmnIT;
-import com.epam.digital.data.platform.bpms.it.util.TestUtils;
-import com.github.tomakehurst.wiremock.WireMockServer;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
-import javax.inject.Inject;
 import org.assertj.core.api.Assertions;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.util.CollectionUtils;
 
 public class SubjectDetailEdrRegistryConnectorDelegateIT extends BaseBpmnIT {
 
-  @Inject
-  @Qualifier("trembitaMockServer")
-  protected WireMockServer trembitaMockServer;
-
-
   @Test
   @Deployment(resources = {"bpmn/testSubjectDetailEdrRegistryConnectorDelegate.bpmn"})
-  public void shouldSearchSubjects() throws IOException, URISyntaxException {
-    mockSubjectDetail();
+  public void shouldSearchSubjects() throws Exception {
+    stubSubjectDetail("/xml/subjectDetailResponse.xml");
 
     var processInstance = runtimeService
         .startProcessInstanceByKey("test_subject_detail_key");
@@ -41,18 +21,5 @@ public class SubjectDetailEdrRegistryConnectorDelegateIT extends BaseBpmnIT {
     List<ProcessInstance> processInstanceList = runtimeService.createProcessInstanceQuery()
         .processInstanceId(processInstance.getId()).list();
     Assertions.assertThat(CollectionUtils.isEmpty(processInstanceList)).isTrue();
-  }
-
-  private void mockSubjectDetail() throws IOException, URISyntaxException {
-    String response = Files.readString(
-        Paths.get(TestUtils.class.getResource("/xml/subjectDetailResponse.xml").toURI()),
-        StandardCharsets.UTF_8);
-
-    trembitaMockServer.addStubMapping(
-        stubFor(post(urlPathEqualTo("/trembita-mock-server"))
-            .withRequestBody(matching(".*SubjectDetail.*"))
-            .willReturn(aResponse().withStatus(200)
-                .withHeader("Content-Type", "text/xml")
-                .withBody(response))));
   }
 }
--- business-process-management/src/it/java/com/epam/digital/data/platform/bpms/it/camunda/bpmn/BaseBpmnIT.java
+++ business-process-management/src/it/java/com/epam/digital/data/platform/bpms/it/camunda/bpmn/BaseBpmnIT.java
@@ -5,6 +5,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.delete;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.matching;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.put;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
@@ -12,6 +13,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.task;
 
+import com.epam.digital.data.platform.bpms.api.constant.Constants;
 import com.epam.digital.data.platform.bpms.delegate.ceph.CephKeyProvider;
 import com.epam.digital.data.platform.bpms.it.BaseIT;
 import com.epam.digital.data.platform.bpms.it.builder.StubData;
@@ -23,11 +25,16 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.MappingBuilder;
 import java.io.IOException;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import javax.inject.Inject;
 import org.assertj.core.api.Assertions;
+import org.camunda.bpm.engine.rest.dto.VariableValueDto;
+import org.camunda.bpm.engine.rest.dto.runtime.StartProcessInstanceDto;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.junit.After;
 import org.junit.Before;
@@ -51,6 +58,9 @@ public abstract class BaseBpmnIT extends BaseIT {
   @Inject
   @Qualifier("userSettingsWireMock")
   protected WireMockServer userSettingsWireMock;
+  @Inject
+  @Qualifier("trembitaMockServer")
+  protected WireMockServer trembitaMockServer;
   @Value("${ceph.bucket}")
   protected String cephBucketName;
 
@@ -216,6 +226,26 @@ public abstract class BaseBpmnIT extends BaseIT {
     digitalSignatureMockServer.addStubMapping(stubFor(mappingBuilder));
   }
 
+  protected void stubSearchSubjects(String responseXmlFilePath) throws Exception {
+    stubTrembita(responseXmlFilePath, "SearchSubjects");
+  }
+
+  protected void stubSubjectDetail(String responseXmlFilePath) throws Exception {
+    stubTrembita(responseXmlFilePath, "SubjectDetail");
+  }
+
+  private void stubTrembita(String responseXmlFilePath, String serviceCode) throws Exception {
+    String response = Files.readString(
+        Paths.get(TestUtils.class.getResource(responseXmlFilePath).toURI()), StandardCharsets.UTF_8);
+
+    trembitaMockServer.addStubMapping(
+        stubFor(post(urlPathEqualTo("/trembita-mock-server"))
+            .withRequestBody(matching(String.format(".*%s.*", serviceCode)))
+            .willReturn(aResponse().withStatus(200)
+                .withHeader("Content-Type", "text/xml")
+                .withBody(response))));
+  }
+
   protected void assertCephContent() {
     expectedCephStorage.forEach((key, value) -> {
       var actualMap = cephService.getFormData(key);
@@ -251,6 +281,25 @@ public abstract class BaseBpmnIT extends BaseIT {
         "{}", Map.class, token);
 
     return (String) result.get("id");
+  }
+
+  protected String startProcessInstanceWithStartFormAndGetId(String processDefinitionKey,
+      String startFormCephKey, String token) throws JsonProcessingException {
+    var result = startProcessInstanceWithStartForm(processDefinitionKey, startFormCephKey, token);
+    return (String) result.get("id");
+  }
+
+  protected Map startProcessInstanceWithStartForm(String processDefinitionKey,
+      String startFormCephKey, String token) throws JsonProcessingException {
+    var startProcessInstanceDto = new StartProcessInstanceDto();
+    var variableValueDto = new VariableValueDto();
+    variableValueDto.setValue(startFormCephKey);
+    startProcessInstanceDto.setVariables(
+        Map.of(Constants.BPMS_START_FORM_CEPH_KEY_VARIABLE_NAME, variableValueDto));
+
+     return postForObject(
+        String.format("api/process-definition/key/%s/start", processDefinitionKey),
+        objectMapper.writeValueAsString(startProcessInstanceDto), Map.class, token);
   }
 
   protected void assertWaitingActivity(ProcessInstance processInstance,
