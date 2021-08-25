791fb220c2a2bf2916fdcf94b6ddd44b757a3169
Oleh_Soklakov <oleh_soklakov@epam.com>
2021-08-25 14:05:18 +0300

    [EDP-DDM-7793] read-personnel-data.bpmn

---
+++ business-process-management/src/it/java/com/epam/digital/data/platform/bpms/it/camunda/bpmn/ReadStaffBpmnIT.java
@@ -0,0 +1,75 @@
+package com.epam.digital.data.platform.bpms.it.camunda.bpmn;
+
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
+
+import com.epam.digital.data.platform.bpms.it.builder.StubData;
+import com.epam.digital.data.platform.integration.ceph.dto.FormDataDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests;
+import org.junit.Test;
+import org.springframework.http.HttpMethod;
+
+public class ReadStaffBpmnIT extends BaseBpmnIT {
+
+  @Test
+  public void test() throws JsonProcessingException {
+    var staffId = "02e68684-1335-47f0-9bd6-17d937267527";
+    var labId = "3758f3e6-937a-4ef9-a8b6-c95671241abd";
+    var researchId = "1238f3e6-937a-4ef9-a8b6-c95671241123";
+
+    stubDataFactoryRequest(StubData.builder()
+        .httpMethod(HttpMethod.GET)
+        .headers(Map.of("X-Access-Token", testUserToken))
+        .resource("staff")
+        .resourceId(staffId)
+        .response("/json/update-staff/getStaffById.json")
+        .build());
+    stubDataFactoryRequest(StubData.builder()
+        .httpMethod(HttpMethod.GET)
+        .headers(Map.of("X-Access-Token", testUserToken))
+        .resource("laboratory")
+        .resourceId(labId)
+        .response("/json/update-staff/laboratoryByIdResponse.json")
+        .build());
+    stubDataFactoryRequest(StubData.builder()
+        .httpMethod(HttpMethod.GET)
+        .headers(Map.of("X-Access-Token", testUserToken))
+        .resource("staff-status")
+        .resourceId("cc974d44-362c-4caf-8a99-67780635ca68")
+        .response("/json/update-staff/getStaffStatusById.json")
+        .build());
+    stubDataFactoryRequest(StubData.builder()
+        .httpMethod(HttpMethod.GET)
+        .headers(Map.of("X-Access-Token", testUserToken))
+        .resource("research")
+        .resourceId(researchId)
+        .response("/json/read-staff/researchByIdResponse.json")
+        .build());
+
+    //start process
+    var startFormCephKey = "startFormCephKey";
+    var data = new LinkedHashMap<String, Object>();
+    data.put("staff", Map.of("staffId", staffId));
+    data.put("laboratory", Map.of("laboratoryId", labId));
+    cephService.putFormData(startFormCephKey, FormDataDto.builder().data(data).build());
+    var processInstanceId = startProcessInstanceWithStartFormAndGetId("read-personnel-data-bp",
+        "startFormCephKey", testUserToken);
+    var processInstance = runtimeService.createProcessInstanceQuery()
+        .processInstanceId(processInstanceId).singleResult();
+
+    BpmnAwareTests.assertThat(processInstance)
+        .isWaitingAt("Activity_read-personnel-bp-read-personnel");
+    completeTask("Activity_read-personnel-bp-read-personnel", processInstanceId,
+        "/json/read-staff/Activity_read-personnel-bp-read-personnel.json");
+
+    //then
+    BpmnAwareTests.assertThat(processInstance)
+        .hasPassed("Activity_read-personnel-bp-read-personnel")
+        .isEnded();
+
+    assertThat(processInstance).variables().containsKey("sys-var-process-completion-result");
+    assertThat(processInstance).variables().containsValue("Дані про кадровий склад відображені");
+  }
+}
