440a62b89801992fdc036305ddabd4899045c999
Ruslan_Lesyk <Ruslan_Lesyk@epam.com>
2021-10-04 18:13:36 +0300

    [MDTUDDM-9488] fixed log level
    Change-Id: Idb657c3d3893c8381b7b8e4fefc778c6766a50f9

--- ddm-bpm-rest/src/main/java/com/epam/digital/data/platform/bpms/rest/exception/mapper/UserDataValidationExceptionMapper.java
+++ ddm-bpm-rest/src/main/java/com/epam/digital/data/platform/bpms/rest/exception/mapper/UserDataValidationExceptionMapper.java
@@ -5,18 +5,21 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
 
 /**
  * The class represents an implementation of {@link ExceptionMapper<ValidationException>} that is
  * used to map {@link ValidationException} to response
  */
+@Slf4j
 @Provider
 public class UserDataValidationExceptionMapper implements
     ExceptionMapper<ValidationException> {
 
   @Override
   public Response toResponse(ValidationException ex) {
+    log.warn("User data validation error", ex);
     return Response.status(HttpStatus.SC_UNPROCESSABLE_ENTITY).entity(ex)
         .type(MediaType.APPLICATION_JSON).build();
   }
