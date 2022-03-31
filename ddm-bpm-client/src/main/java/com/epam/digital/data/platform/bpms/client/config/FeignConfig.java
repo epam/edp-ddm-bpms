5338edcd97b5dfc507eac08080fea5e133b75c85
Yevhen_Zvarych <yevhen_zvarych@epam.com>
2022-03-31 19:26:38 +0300

    [EDP-DDM-8109] - Disabled csrf for requests
    Change-Id: I7b61d343987eccad0fdad029a678b1492cf0dfa6

--- ddm-bpm-client/src/main/java/com/epam/digital/data/platform/bpms/client/config/FeignConfig.java
+++ ddm-bpm-client/src/main/java/com/epam/digital/data/platform/bpms/client/config/FeignConfig.java
@@ -16,16 +16,11 @@
 
 package com.epam.digital.data.platform.bpms.client.config;
 
-import com.epam.digital.data.platform.bpms.api.dto.enums.PlatformHttpHeader;
 import com.epam.digital.data.platform.bpms.client.BaseFeignClient;
-import feign.RequestInterceptor;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.UUID;
 
 /**
  * The class represents a configuration for all feign clients.
@@ -36,14 +31,4 @@ import java.util.UUID;
 @EnableFeignClients(basePackageClasses = BaseFeignClient.class)
 public class FeignConfig {
 
-  private static final String XSRF_COOKIE_NAME = "XSRF-TOKEN";
-
-  @Bean
-  public RequestInterceptor csrfRequestInterceptor() {
-    return requestTemplate -> {
-      var requestXsrfToken = UUID.randomUUID().toString();
-      requestTemplate.header(PlatformHttpHeader.X_XSRF_TOKEN.getName(), requestXsrfToken);
-      requestTemplate.header("Cookie", String.format("%s=%s", XSRF_COOKIE_NAME, requestXsrfToken));
-    };
-  }
 }
