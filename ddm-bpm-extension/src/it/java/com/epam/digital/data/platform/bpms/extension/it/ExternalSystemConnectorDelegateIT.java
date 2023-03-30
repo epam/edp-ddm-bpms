0b58fd8585fc3102976ffa4a4ea50302aafec015
eduard_kuznetsov <eduard_kuznetsov@epam.com>
2023-03-30 16:20:42 +0300

    [EDP-DDM-24718] added caching for external system bearer token
    Change-Id: I6c3ac528eb79ba85a51843272e16e124ce4adbb8

--- ddm-bpm-extension/src/it/java/com/epam/digital/data/platform/bpms/extension/it/ExternalSystemConnectorDelegateIT.java
+++ ddm-bpm-extension/src/it/java/com/epam/digital/data/platform/bpms/extension/it/ExternalSystemConnectorDelegateIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 EPAM Systems.
+ * Copyright 2023 EPAM Systems.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,19 +24,38 @@ import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertThrows;
 
+import com.epam.digital.data.platform.bpms.extension.config.properties.ExternalSystemConfigurationProperties.AuthenticationConfiguration.AuthenticationType;
 import com.epam.digital.data.platform.bpms.extension.exception.AuthConfigurationException;
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.nimbusds.jose.JOSEObjectType;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.crypto.ECDSASigner;
+import com.nimbusds.jose.jwk.Curve;
+import com.nimbusds.jose.jwk.gen.ECKeyGenerator;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import java.sql.Date;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
 import javax.inject.Inject;
+import lombok.SneakyThrows;
+import org.assertj.core.api.Assertions;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.CacheManager;
 
 public class ExternalSystemConnectorDelegateIT extends BaseIT {
 
   @Inject
   @Qualifier("external-system")
   private WireMockServer externalSystem1;
+
+  @Inject
+  private CacheManager cacheManager;
 
   @Test
   @Deployment(resources = "bpmn/connector/testExternalSystemConnectorDelegate.bpmn")
@@ -95,6 +114,27 @@ public class ExternalSystemConnectorDelegateIT extends BaseIT {
     externalSystem1.addStubMapping(
         stubFor(get(urlPathEqualTo("/api/get"))
             .withHeader("Authorization", equalTo("Bearer bearer-token"))
+            .willReturn(aResponse().withStatus(200))));
+
+    var processInstance = runtimeService
+        .startProcessInstanceByKey("external_system_partner_token_auth");
+
+    BpmnAwareTests.assertThat(processInstance).isEnded();
+  }
+
+  @Test
+  @Deployment(resources = "bpmn/connector/testExternalSystemConnectorDelegate.bpmn")
+  public void externalSystemCachedPartnerTokenAuth() {
+    var jwt = generateJWT(Instant.now().plus(1, ChronoUnit.HOURS));
+    var cache = cacheManager.getCache(AuthenticationType.AUTH_TOKEN_BEARER.getCode());
+
+    Assertions.assertThat(cache).isNotNull();
+
+    cache.put(externalSystem1.baseUrl(), jwt);
+
+    externalSystem1.addStubMapping(
+        stubFor(get(urlPathEqualTo("/api/get"))
+            .withHeader("Authorization", equalTo("Bearer " + jwt))
             .willReturn(aResponse().withStatus(200))));
 
     var processInstance = runtimeService
@@ -182,5 +222,26 @@ public class ExternalSystemConnectorDelegateIT extends BaseIT {
         .startProcessInstanceByKey("external_system_without_operations"));
     assertThat(ex.getMessage()).isEqualTo(
         "Operation operation1 in external-system system-without-operations not configured");
+  }
+
+  @SneakyThrows
+  private String generateJWT(Instant instant) {
+    var key = new ECKeyGenerator(Curve.SECP256K1)
+        .keyID(UUID.randomUUID().toString())
+        .generate();
+
+    var header = new JWSHeader.Builder(JWSAlgorithm.ES256K)
+        .type(JOSEObjectType.JWT)
+        .keyID(key.getKeyID())
+        .build();
+    var payload = new JWTClaimsSet.Builder()
+        .issuer("http://keycloak:8080")
+        .subject("admin_user")
+        .expirationTime(Date.from(instant))
+        .build();
+
+    var signedJWT = new SignedJWT(header, payload);
+    signedJWT.sign(new ECDSASigner(key.toECPrivateKey()));
+    return signedJWT.serialize();
   }
 }
--- ddm-bpm-extension/src/it/java/com/epam/digital/data/platform/bpms/extension/it/TestDelegateApp.java
+++ ddm-bpm-extension/src/it/java/com/epam/digital/data/platform/bpms/extension/it/TestDelegateApp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 EPAM Systems.
+ * Copyright 2023 EPAM Systems.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,9 +18,11 @@ package com.epam.digital.data.platform.bpms.extension.it;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(scanBasePackages = {"com.epam.digital.data.platform.bpms.extension",
     "com.epam.digital.data.platform.bpms.engine"})
+@EnableCaching
 public class TestDelegateApp {
 
   public static void main(String[] args) {
