ab2d4a6fa1b4d1541fd6fce8fc4de2860f4e8f02
Lypivets Oleksandr <oleksandr_lypivets@epam.com>
2023-07-13 10:07:42 +0300

    [MDTUDDM-27153] [MDTUDDM-27159] added sendUserNotificationByAddressDelegate and configmap for emails blacklist
    Change-Id: Ib9e6a6d7781a057c5839d7507eaebdec729560d8

--- ddm-bpm-extension/src/it/java/com/epam/digital/data/platform/bpms/extension/it/config/TestNotificationsStarterConfig.java
+++ ddm-bpm-extension/src/it/java/com/epam/digital/data/platform/bpms/extension/it/config/TestNotificationsStarterConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 EPAM Systems.
+ * Copyright 2023 EPAM Systems.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +16,12 @@
 
 package com.epam.digital.data.platform.bpms.extension.it.config;
 
+import com.epam.digital.data.platform.bpms.extension.config.BlackListEmailDomainsProperties;
+import com.epam.digital.data.platform.bpms.extension.delegate.notification.SendUserNotificationByAddress;
 import com.epam.digital.data.platform.bpms.extension.delegate.notification.SendUserNotificationDelegate;
 import com.epam.digital.data.platform.starter.notifications.facade.UserNotificationFacade;
 import org.mockito.Mockito;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -36,5 +39,20 @@ public class TestNotificationsStarterConfig {
     public SendUserNotificationDelegate sendUserNotificationDelegate(
         UserNotificationFacade notificationFacade) {
         return new SendUserNotificationDelegate(notificationFacade);
+    }
+
+    @Bean
+    public UserNotificationFacade notificationFacadeByAddress() {
+        return Mockito.mock(UserNotificationFacade.class);
+    }
+    @Bean(name = SendUserNotificationByAddress.DELEGATE_NAME)
+    public SendUserNotificationByAddress sendUserNotificationByAddress() {
+        return new SendUserNotificationByAddress(notificationFacadeByAddress(), blackList());
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "validation.email.blacklist")
+    public BlackListEmailDomainsProperties blackList() {
+        return new BlackListEmailDomainsProperties();
     }
 }
